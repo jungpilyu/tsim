0402c62a094e7c1d34a70dc69fa14a21d2ecc42c
Jungpil YU <jungpil.yu@gmail.com>
2020-07-09 02:01:48 +0900

    add parameterEvent handler

--- tsim/commander.py
+++ tsim/commander.py
@@ -1,21 +1,13 @@
 """ROS2 node to publish direction messages from keyboard.
 """
 import rclpy
-USE = 'termios'
-try:
-    import termios
-except ImportError:
-    try:
-        import msvcrt
-    except ImportError:
-        rclpy.logging.get_logger('top').info('Import Error, I quit!')
-    else:
-        USE = 'msvcrt'
 import sys
 from std_msgs.msg import String
 from rcl_interfaces.msg import ParameterDescriptor
 from rcl_interfaces.msg import ParameterType
 from rcl_interfaces.msg import SetParametersResult
+from rcl_interfaces.msg import ParameterEvent
+from rcl_interfaces.msg import FloatingPointRange
 import select
 import termios
 import tty
@@ -26,46 +18,57 @@ def main():
     rclpy.init()
     # 2. Create one or more nodes
     node = rclpy.create_node('commander')
+    fp_range = FloatingPointRange(
+        from_value = 0.2,
+        to_value = 2.0,
+        step = 0.1
+    )
     pd = ParameterDescriptor(
         name = 'parameter in commander',
         type = ParameterType.PARAMETER_DOUBLE,
         description = 'set key-in period in seconds',
         read_only = False,
-#        floating_point_range = 0.1, # from_value, to_value, step
+        floating_point_range = [fp_range]
     )
-    node.declare_parameter('key-in-period', value=0.2,
-        descriptor=pd, ignore_override=False)
+    param = node.declare_parameter('key-in-period', value = 0.2, 
+                        descriptor = pd, ignore_override=False)
     # 3. Process node callbacks
     publisher = node.create_publisher(String, 'keys', 10)
     
     def unix_callback():
         fd = sys.stdin.fileno()
-        old = termios.tcgetattr(fd) # get the tty attributes
+        old = termios.tcgetattr(fd)
         tty.setcbreak(fd, tty.TCSANOW) 
         timeout = 0
-        node.get_logger().info('Please key!')
+        node.get_logger().info('Please key in!')
         read_ready = select.select([sys.stdin], [], [], timeout)[0]
         if read_ready == [sys.stdin]:
             msg = String()
             msg.data = sys.stdin.read(1)
             publisher.publish(msg)
         termios.tcsetattr(fd, termios.TCSANOW, old)
-    def win_callback():
-        if msvcrt.kbhit() == False:
-            msg = String()
-            msg.data = msvcrt.getch()
-            publisher.publish(msg)
-    def on_parameter(param_list):
-        nonlocal timer
-        period = param_list[0].get_parameter_value().double_value
-        node.destroy_timer(timer)
-        timer = node.create_timer(timer_period_sec = period, callback = callback)
-        return SetParametersResult(successful=True)
 
-    callback = unix_callback if USE == 'termios' else win_callback
-    period = node.get_parameter('key-in-period').get_parameter_value().double_value
-    node.add_on_set_parameters_callback(on_parameter)
-    timer = node.create_timer(timer_period_sec = period, callback = callback)
+    def before_parameter(param_list: [rclpy.parameter.Parameter]):
+        nonlocal timer
+        p_list = [its for its in param_list if its.name == 'key-in-period']
+        period = p_list[0].get_parameter_value().double_value
+        node.destroy_timer(timer)
+        timer = node.create_timer(timer_period_sec = period, callback = unix_callback)
+        return SetParametersResult(successful=True)
+    node.add_on_set_parameters_callback(before_parameter)
+    
+    def after_parameter(msg: ParameterEvent):
+        if msg.node != '/commander':
+            return
+        param_msg_list = [its for its in msg.changed_parameters if its.name == 'key-in-period']
+        name = param_msg_list[0].name
+        value = param_msg_list[0].value.double_value
+        node.get_logger().info(
+            'parameter \'{}\' = {} sec.'.format(name, value))
+    node.create_subscription(ParameterEvent, 'parameter_events', after_parameter, 10)
+
+    period = param.get_parameter_value().double_value
+    timer = node.create_timer(timer_period_sec = period, callback = unix_callback)
     node.get_logger().info('Publishing keystrokes.')
 
     rclpy.spin(node)
